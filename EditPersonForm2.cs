a6588d19927d015015724fadbac19ed25cc88c65
Evgeny Kotsiuk <37482262+Olduin@users.noreply.github.com>
2020-12-14 15:02:32 +0400

    Add files via upload

---
+++ EditPersonForm2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestEntityFramework
+{
+    public partial class EditPersonForm2 : Form
+    {
+        public EditPersonForm2()
+        {
+            InitializeComponent();
+        }
+
+ 
+    }
+}
---
+++ EditUserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestEntityFramework
+{
+    public class EditUserContext
+    {
+        public User User { get; set; }
+
+        public List<Person> Persons { get; set; }
+
+        public List<Role> Roles { get; set; }
+
+    }
+}
